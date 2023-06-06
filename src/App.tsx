9a352705a2e5ee33ce23cd9d7d8a6d3f40f4a54c
fenglong_ng <fenglong1502@hotmail.com>
2023-06-06 08:17:32 +0800

    Add useAlert hook with context

--- src/App.tsx
+++ src/App.tsx
@@ -17,27 +17,30 @@ import ViewPlacePage from './pages/places/ViewPlace';
 import AddPlace from './pages/places/AddPlace';
 import ForgetPassword from './pages/forget-password-page';
 import { AuthProvider } from './context/AuthContext';
+import { AlertProvider } from './context/AlertContext';
 
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path={ROUTES.LANDING_PAGE} element={<LandingPage />} />
-            <Route path={ROUTES.TRIPS} element={<ComingSoonPage />} />
-            <Route path={ROUTES.ACTIVITIES} element={<ComingSoonPage />} />
-            <Route path={ROUTES.ADD_PLACES} element={<AddPlace />} />
-            <Route path={ROUTES.VIEW_PLACES} element={<ViewPlacePage />} />
-            <Route path={ROUTES.PLACES} element={<ComingSoonPage />} />
-            <Route path={ROUTES.SETTING} element={<ComingSoonPage />} />
-            <Route path={ROUTES.ALL} element={<PageNotFound />} />
-          </Route>
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-          <Route path={ROUTES.FORGET_PASSWORD} element={<ForgetPassword />} />
-        </Routes>
-      </BrowserRouter>
+      <AlertProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path={ROUTES.LANDING_PAGE} element={<LandingPage />} />
+              <Route path={ROUTES.TRIPS} element={<ComingSoonPage />} />
+              <Route path={ROUTES.ACTIVITIES} element={<ComingSoonPage />} />
+              <Route path={ROUTES.ADD_PLACES} element={<AddPlace />} />
+              <Route path={ROUTES.VIEW_PLACES} element={<ViewPlacePage />} />
+              <Route path={ROUTES.PLACES} element={<ComingSoonPage />} />
+              <Route path={ROUTES.SETTING} element={<ComingSoonPage />} />
+              <Route path={ROUTES.ALL} element={<PageNotFound />} />
+            </Route>
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+            <Route path={ROUTES.FORGET_PASSWORD} element={<ForgetPassword />} />
+          </Routes>
+        </BrowserRouter>
+      </AlertProvider>
     </AuthProvider>
   );
 }
---
+++ src/context/AlertContext.tsx
@@ -0,0 +1,54 @@
+import { ALERT_TYPE } from '../components/alert/Alert';
+import { createContext, useState, useCallback } from 'react';
+
+export interface AlertContextData {
+  alertType: ALERT_TYPE | null;
+  alertHeader: string | null;
+  alertDescription: string | null;
+  displayAlert: (
+    alertType: ALERT_TYPE,
+    alertHeader: string,
+    alertDescription: string
+  ) => void;
+  resetAlert: () => void;
+}
+
+export const AlertContext = createContext<AlertContextData>({
+  alertType: null,
+  alertHeader: null,
+  alertDescription: null,
+  displayAlert: () => {},
+  resetAlert: () => {},
+});
+
+export const AlertProvider = ({ children }: { children: React.ReactNode }) => {
+  const [alertType, setAlertType] = useState<ALERT_TYPE | null>(null);
+  const [alertHeader, setAlertHeader] = useState<string | null>(null);
+  const [alertDescription, setAlertDescription] = useState<string | null>(null);
+
+  const displayAlert = useCallback(
+    (alertType: ALERT_TYPE, alertHeader: string, alertDescription: string) => {
+      setAlertType(alertType);
+      setAlertHeader(alertHeader);
+      setAlertDescription(alertDescription);
+    },
+    []
+  );
+
+  const resetAlert = useCallback(() => {
+    setAlertType(null);
+    setAlertHeader(null);
+    setAlertDescription(null);
+  }, []);
+
+  const value = {
+    alertType,
+    alertHeader,
+    alertDescription,
+    displayAlert,
+    resetAlert,
+  };
+  return (
+    <AlertContext.Provider value={value}>{children}</AlertContext.Provider>
+  );
+};
