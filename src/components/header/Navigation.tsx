8fd86f295ae839d1a0705fcfd86a1d50e9adcb84
fenglong_ng <fenglong1502@hotmail.com>
2023-06-19 07:57:13 +0800

    Edit active tab css

--- src/components/header/Navigation.tsx
+++ src/components/header/Navigation.tsx
@@ -1,30 +1,49 @@
+import { useLocation } from 'react-router-dom';
 import * as ROUTES from './../../constants/routes';
 
 const Navigation = () => {
+  const location = useLocation();
+
   return (
     <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
       {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
       <a
         href={ROUTES.LANDING_PAGE}
-        className="inline-flex items-center border-b-2 border-blue-primary px-1 pt-1 text-sm font-medium text-gray-900"
+        className={`inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium text-gray-900 ${
+          location.pathname === ROUTES.LANDING_PAGE
+            ? ' border-blue-primary'
+            : ' border-transparent '
+        }`}
       >
         Explore
       </a>
       <a
         href={ROUTES.TRIPS}
-        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+        className={`inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 ${
+          location.pathname === ROUTES.TRIPS
+            ? ' border-blue-primary'
+            : ' border-transparent '
+        }`}
       >
         Trips
       </a>
       <a
         href={ROUTES.ACTIVITIES}
-        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+        className={`inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 ${
+          location.pathname === ROUTES.ACTIVITIES
+            ? ' border-blue-primary'
+            : ' border-transparent '
+        }`}
       >
         Activities
       </a>
       <a
         href={ROUTES.PLACES}
-        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+        className={`inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700 ${
+          location.pathname === ROUTES.PLACES
+            ? ' border-blue-primary'
+            : ' border-transparent '
+        }`}
       >
         Places
       </a>
