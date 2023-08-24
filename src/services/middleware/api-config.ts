5cd40f7747451696bf63e44e765c0a962f85efb5
fenglong_ng <fenglong1502@hotmail.com>
2023-08-25 07:47:53 +0800

    Add retry and refresh token api interceptor

--- src/services/middleware/api-config.ts
+++ src/services/middleware/api-config.ts
@@ -1,6 +1,19 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosError, AxiosRequestConfig } from 'axios';
+
+type FailedQueue = Array<{
+  requestConfig: AxiosRequestConfig;
+  resolve: (value?: any) => void;
+  reject: (reason?: any) => void;
+}>;
+
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+  _retry?: boolean;
+}
 
 const baseURL: string = process.env.REACT_APP_IKOU_API_BASEURL || '';
+
+const getToken = () => localStorage.getItem('access_token');
+const setToken = (token: string) => localStorage.setItem('access_token', token);
 
 // 1. Create a new Axios instance
 const api: AxiosInstance = axios.create({
@@ -14,7 +27,7 @@ const api: AxiosInstance = axios.create({
 // 2. Request Interceptor
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('access_token'); // Adjust according to where you store the JWT token
+    const token = getToken();
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
@@ -24,5 +37,97 @@ api.interceptors.request.use(
     Promise.reject(error);
   }
 );
+
+// // 3. Response Interceptor
+// Response interceptor
+let isRefreshing = false;
+let failedQueue: FailedQueue = [];
+
+const processQueue = (error: any, token?: string) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      if (prom.requestConfig.headers) {
+        prom.requestConfig.headers['Authorization'] = 'Bearer ' + token;
+      } else {
+        prom.requestConfig.headers = { Authorization: 'Bearer ' + token };
+      }
+      api(prom.requestConfig).then(prom.resolve).catch(prom.reject);
+    }
+  });
+
+  failedQueue = [];
+};
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response) {
+        const retryStatusCodes = [502, 503, 504];
+
+        if (
+          retryStatusCodes.includes(error.response.status) &&
+          !error.config.__isRetryRequest
+        ) {
+          error.config.__isRetryRequest = true;
+          return api.request(error.config);
+        } else if (error.response.status === 401 && !error.config._retry) {
+          return handle401Error(error);
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+const handle401Error = (error: AxiosError) => {
+  const originalRequest = error.config as CustomAxiosRequestConfig;
+
+  if (!originalRequest) {
+    // Handle or log the error appropriately
+    console.error('Axios config is undefined', error);
+    return Promise.reject(error);
+  }
+
+  if (isRefreshing) {
+    return new Promise<AxiosError>((resolve, reject) => {
+      failedQueue.push({ requestConfig: originalRequest, resolve, reject });
+    });
+  }
+
+  originalRequest._retry = true;
+  isRefreshing = true;
+
+  const refreshToken = localStorage.getItem('refresh_token');
+
+  return api
+    .post('/auth/refresh-token', { refreshToken })
+    .then(({ data }) => {
+      setToken(data.access_token);
+      api.defaults.headers['Authorization'] = 'Bearer ' + data.access_token;
+      if (originalRequest.headers)
+        originalRequest.headers['Authorization'] =
+          'Bearer ' + data.access_token;
+      else
+        originalRequest.headers = {
+          Authorization: 'Bearer ' + data.access_token,
+        };
+      processQueue(null);
+      return api(originalRequest);
+    })
+    .catch((err: AxiosError) => {
+      processQueue(err);
+      return Promise.reject(err);
+    })
+    .finally(() => {
+      isRefreshing = false;
+    });
+};
+
 
 export default api;
