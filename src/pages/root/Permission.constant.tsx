f0cecf8d729bc4f7a7dc2a2d2fb29a7b041b1f5c
fenglong_ng <fenglong1502@hotmail.com>
2023-09-17 12:00:00 +0800

    fix eslint

--- src/pages/root/Permission.constant.tsx
+++ src/pages/root/Permission.constant.tsx
@@ -1,5 +1,5 @@
 // Consider using number enum to prevent user from reading it
 export const PERMISSION = {
-  CREATE_USER: "CREATE_USER",
-  CREATE_PLACE: "CREATE_PLACE",
+  CREATE_USER: 'CREATE_USER',
+  CREATE_PLACE: 'CREATE_PLACE',
 };
